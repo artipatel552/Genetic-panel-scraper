3f0de4dab2d53a2070c3dda2670be57e852946e9
artipatel552 <artipatel552@gmail.com>
2026-05-28 11:28:02 -0500

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,16 +1,29 @@
+print("loading scrapers package")
+
 from .invitae import InvitaeScraper
+print("invitae ok")
+
 from .ambry import AmbryScraper
+print("ambry ok")
+
 from .genedx import GeneDxScraper
+print("genedx ok")
+
 from .myriad import MyriadScraper
+print("myriad ok")
+
 from .blueprint import BlueprintScraper
+print("blueprint ok")
+
 from .natera_fulgent import NateraScraper, FulgentScraper
+print("natera_fulgent ok")
 
 ALL_SCRAPERS = [
-    InvitaeScraper,
-    AmbryScraper,
-    GeneDxScraper,
-    MyriadScraper,
-    BlueprintScraper,
-    NateraScraper,
-    FulgentScraper,
+    InvitaeScraper,
+    AmbryScraper,
+    GeneDxScraper,
+    MyriadScraper,
+    BlueprintScraper,
+    NateraScraper,
+    FulgentScraper,
 ]
