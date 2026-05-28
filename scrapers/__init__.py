e56eb8ca2d7f308790e3f125c3de556d92bcd558
artipatel552 <artipatel552@gmail.com>
2026-05-28 11:21:16 -0500

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,9 +1,9 @@
-from scrapers.invitae import InvitaeScraper
-from scrapers.ambry import AmbryScraper
-from scrapers.genedx import GeneDxScraper
-from scrapers.myriad import MyriadScraper
-from scrapers.blueprint import BlueprintScraper
-from scrapers.natera_fulgent import NateraScraper, FulgentScraper
+from .invitae import InvitaeScraper
+from .ambry import AmbryScraper
+from .genedx import GeneDxScraper
+from .myriad import MyriadScraper
+from .blueprint import BlueprintScraper
+from .natera_fulgent import NateraScraper, FulgentScraper
 
 ALL_SCRAPERS = [
     InvitaeScraper,
